49b2b158aa08a671ecb073854d31621b6f9b17a8
Ho Tan Hung <tanhungho2002@gmail.com>
2023-09-17 18:29:33 +0700

    23. Xóa 1 vài thành phần component bị xóa ở router

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,11 +5,6 @@ import SongFavorite from '~/pages/SongFavorite';
 import Top100 from '~/pages/Top100';
 import TopicAndType from '~/pages/TopicAndType';
 import ListeningNear from '~/pages/ListeningNear';
-import Admin from '~/Admin/Admin';
-import CRUDNewRelease from '~/Admin/pages/CRUDNewRelease/CRUDNewRelease';
-import CRUDSong from '~/Admin/pages/CRUDSong/CRUDNewSong';
-import DashBoard from '~/Admin/pages/Dashboard/Dashboard';
-
 import configs from '~/Configs/config';
 
 const publicRoutes = [
@@ -20,10 +15,6 @@ const publicRoutes = [
     { path: configs.router.top100, component: Top100 },
     { path: configs.router.topicAndType, component: TopicAndType },
     { path: configs.router.listeningNear, component: ListeningNear },
-    { path: configs.router.admin, component: Admin, layout: null },
-    { path: configs.router.crudsong, component: CRUDSong, layout: null },
-    { path: configs.router.crudnewreleases, component: CRUDNewRelease, layout: null },
-    { path: configs.router.dashboard, component: DashBoard, layout: null },
 ];
 const privateRoutes = [];
 
